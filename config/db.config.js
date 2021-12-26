37d275a302af3973604741fa3273fe6bcd3ac5b6
Dlray89 <dlrayjr89@gmail.com>
2021-12-26 15:13:17 -0500

    inti

--- config/db.config.js
+++ config/db.config.js
@@ -1,17 +1,13 @@
-require("dotenv").config();
-
 module.exports = {
-  HOST: 'ec2-18-213-179-70.compute-1.amazonaws.com',
-  USER: 'oowyvdywxpkfts',
-  PASSWORD: '321ff4565ac1a08508e8f7b9c5392dc89da1f5c48bb905d2cc57145d986dd73d',
-  DB: 'd2nuargric33g5',
-  dialect: process.env.DB_DIALECT,
-  PORT: process.env.DB_PORT,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-    },
-  
-};
+    HOST: 'localhost',
+    USER: 'postgres',
+    PASSWORD: "1234",
+    DB: 'postgres',
+    dialect: 'postgres',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+}
